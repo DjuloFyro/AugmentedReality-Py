55fef033b2b005c4f2de35bc32d2cde858f442b1
julian.gil <juliangil2424@yahoo.fr>
2024-06-03 20:16:40 +0200

    add: work on server

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode, RTCConfiguration
+import cv2
+import av
+from aruco_detector import find_aruco_markers
 
-st.title("WebRTC Camera Stream with Streamlit")
+st.title("Camera Stream with OpenCV and Streamlit")
 
+class VideoProcessor(VideoProcessorBase):
+    def __init__(self):
+        self.camera_active = False
+
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        # Apply the aruco marker detection
+        find_aruco_markers(img)
+        
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+# RTC configuration
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
-# Using WebRTC to stream video from the camera
+# Streamlit-webrtc component with RTC configuration
 webrtc_ctx = webrtc_streamer(
     key="WYH",
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
+    video_processor_factory=VideoProcessor,
     async_processing=False,
 )
-
-if webrtc_ctx.video_receiver:
-    while True:
-        frame = webrtc_ctx.video_receiver.get_frame()
-        st.image(frame.to_ndarray(format="bgr24"), channels="BGR")
-else:
-    st.write("Click the button to start the camera.")
